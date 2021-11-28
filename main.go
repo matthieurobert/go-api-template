6b7d37f6f485e125b07918858dfa2ba217fe2319
Matthieu Robert <matthieurobert82@gmail.com>
2021-11-28 20:09:20 +0000

    feat: Add basic http route

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/matthieurobert/go-api-template/config"
 )
 
 func main() {
 	config.Init()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+	http.ListenAndServe(":8000", nil)
 
 	fmt.Println("It works !!")
 }
